57611d15fd39bfa1c720e8f2b013ef6203924317
Trung Ngo <ndtrung4419@gmail.com>
2017-10-21 19:06:24 +0300

    Generate simple punchcard graphs

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, abort
+from datetime import timedelta 
 
 from persistence import get_reports, get_report
 
@@ -11,14 +12,40 @@ def list_reports():
 def upload_data():
     pass
 
+
+def generate_punchcard_matrix(work_sessions):
+    matrix = [[0 for hour in range(0, 24)] for day in range(0, 7)]
+    max_value = 0
+    for start, end in work_sessions:
+        day = start.weekday()
+        while start < end:
+            matrix[day][start.hour] += 1
+            if matrix[day][start.hour] > max_value:
+                max_value = matrix[day][start.hour]
+            
+            start += timedelta(hours=1)
+
+
+    print(max_value)
+    # normalize to the largest value
+    if max_value > 0:
+        for day in range(0, 7):
+            for hour in range(0, 24):
+                matrix[day][hour] /= max_value
+
+    return matrix
+
+
 @app.route("/report/<int:year>/<int:month>")
 def view_report(year, month):
     if (year, month) not in get_reports():
         abort(404)
 
     pay = get_report(year, month)
+    punchcard_matrices = {person_id: generate_punchcard_matrix(pay[person_id]["sessions"]) for person_id in pay}
 
     return render_template("view_report.html", 
         report_name="{}/{}".format(year, month),
         wage=sorted(pay.items(), key=lambda t: t[0]),
-        wage_json=pay)
+        wage_json=pay,
+        punchcards=punchcard_matrices)
